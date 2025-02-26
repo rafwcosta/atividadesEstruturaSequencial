c021cb7df5f1e0fde5dcd3903fee4300d382f365
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-02-26 15:44:35 -0300

    Question 01

---
+++ src/question01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class question01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite o valor de A: ");
+        int a = sc.nextInt();
+        System.out.print("Digite o valor de B: ");
+        int b = sc.nextInt();
+
+        int soma = a + b;
+        System.out.println(a + " + " + b + " = " + soma);
+
+        sc.close();
+    }
+}
