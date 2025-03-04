b3bca6a3a705e08c5382215f21dc96a747b19328
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-03-04 12:28:14 -0300

    Sequential structure review 01

---
+++ revisões/Sequential01.java
@@ -0,0 +1,22 @@
+package revisões;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Sequential01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite A: ");
+        int a = sc.nextInt();
+        System.out.print("Digite B: ");
+        int b = sc.nextInt();
+
+        int soma = a + b;
+
+        System.out.println("Soma: " + soma);
+
+        sc.close();
+    }
+}
