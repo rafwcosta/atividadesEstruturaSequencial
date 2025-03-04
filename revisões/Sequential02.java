0ba8675362d0e9a49d1305c5630b53d5f0015273
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-03-04 12:30:59 -0300

    Sequential structure review 02

---
+++ revisões/Sequential02.java
@@ -0,0 +1,20 @@
+package revisões;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Sequential02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite o valor do raio: ");
+        double raio = sc.nextDouble();
+
+        double area = 3.14159 * (raio * raio);
+
+        System.out.printf("Área do círculo: %.4f%n", area);
+
+        sc.close();
+    }
+}
